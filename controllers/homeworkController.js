355e48d3ed4d77d633c0104a96bc2857333d95c8
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-05-11 13:48:32 +0700

    done

--- controllers/homeworkController.js
+++ controllers/homeworkController.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const Homework = require('../models/Homework');
+const Homework = require('../models/homework');
 
 // Настройка multer для сохранения файлов
 const storage = multer.diskStorage({
